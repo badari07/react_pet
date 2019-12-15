829bad0cdf08d90099ddc707d9577346bcc94e9c
badari07 <badari0793@gmail.com>
2019-12-15 17:38:39 +0530

    rendering data and setup api keys

--- src/component/App.js
+++ src/component/App.js
@@ -1,14 +1,72 @@
 import React from "react";
 import { render } from "react-dom";
+// import pf from "petfinder-client";
+import { Client } from "@petfinder/petfinder-js";
 import Pet from "./Pet";
 
+const client = new Client({
+  apiKey: "l17DIr0dMsFwEx8rrAgZAotWeeaXWK2JP4AHre6HU1bgGLa4rT",
+  secret: "inCZaTd1EQrm10APLT32zgQqteUMnvSgtyBMWRtA"
+});
+
+// const petfinder = pf({
+//   key: process.env.API_KEY,
+//   secret: process.env.API_SECRET
+// });
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    // petfinder.breed.list({ animal: "dog" }).then(console.log, console.error);
+    client.animal
+      .search({ location: "Seattle, WA" })
+      .then(resp => {
+        // Do something with resp.data.organizations
+        let pets;
+        if (resp.data.animals) {
+          if (Array.isArray(resp.data.animals)) {
+            pets = resp.data.animals;
+          } else {
+            pets = [resp.data.animals];
+          }
+        } else {
+          pets = [];
+        }
+        this.setState({
+          pets
+        });
+        console.log(resp);
+      })
+      .catch(function(error) {
+        // Handle the error
+        console.log(`you got ${error}`);
+      });
+  }
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="luna" animal="dog" breed="hanver" />
-        <Pet name="bob" animal="cat" breed="maxi" />
+        <div>
+          {this.state.pets.map(pet => {
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.type}
+                name={pet.name}
+                breed={pet.breeds.primary}
+                loaction={`${pet.contact.address.city}, ${
+                  pet.contact.address.state
+                }`}
+                photos={pet.photos}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
--- src/component/Pet.js
+++ src/component/Pet.js
@@ -1,18 +1,44 @@
 import React from "react";
 
-const Pet = props => {
-  // return React.createElement("div", null, [
-  //   React.createElement("h1", null, props.name),
-  //   React.createElement("h2", null, props.animal),
-  //   React.createElement("h3", null, props.breed)
-  // ]);
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h3>{props.breed}</h3>
-    </div>
-  );
-};
+// const Pet = props => {
+//   // return React.createElement("div", null, [
+//   //   React.createElement("h1", null, props.name),
+//   //   React.createElement("h2", null, props.animal),
+//   //   React.createElement("h3", null, props.breed)
+//   // ]);
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.animal}</h2>
+//       <h3>{props.breed}</h3>
+//     </div>
+//   );
+// };
+
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, photos, loaction } = this.props;
+    // console.log(photos[0].small);
+    let photo;
+    if (photos.length <= 0) {
+      photo = "image is not found";
+    } else {
+      photo = photos[0].small;
+    }
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photo} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal}- {breed}- {loaction}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Pet;
