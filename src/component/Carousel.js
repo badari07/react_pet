27d3d9e21f756b51ff40dbc5c93409127a46f1b7
badari07 <badari0793@gmail.com>
2019-12-24 16:23:31 +0530

    editing details

--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -25,24 +25,35 @@ class Carousel extends Component {
   render() {
     const { photos, active } = this.state;
     // console.log(photos);
-    return (
-      <div className="carousel">
-        <img src={photos[active].medium} alt="primary animal" />
-        <div className="carousel-smaller">
-          {photos.map((photo, index) => (
-            /* eslint-disable-next-line */
-            <img
-              onClick={this.handleIndexClick}
-              key={photo.small}
-              data-index={index}
-              src={photo.small}
-              className={index === active ? "active" : ""}
-              alt="animal thumbnail"
-            />
-          ))}
+    if (!photos[active].medium) {
+      return (
+        <div className="carousel">
+          <img src={photos} alt="primary animal" />
+          <div className="carousel-smaller">
+            <span style={{ color: "red" }}>Image Is Not Found</span>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="carousel">
+          <img src={photos[active].medium} alt="primary animal" />
+          <div className="carousel-smaller">
+            {photos.map((photo, index) => (
+              /* eslint-disable-next-line */
+              <img
+                onClick={this.handleIndexClick}
+                key={photo.small}
+                data-index={index}
+                src={photo.small}
+                className={index === active ? "active" : ""}
+                alt="animal thumbnail"
+              />
+            ))}
+          </div>
+        </div>
+      );
+    }
   }
 }
 
